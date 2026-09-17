f6f7e7130a521608c5324051702e92f31410206b
gokturktuna52 <gokturktuna7@gmail.com>
2026-09-17 15:34:41 +0000

    FjOS Launcher 0.1

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.fjos.launcher"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.fjos.launcher"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1"
+    }
+}
